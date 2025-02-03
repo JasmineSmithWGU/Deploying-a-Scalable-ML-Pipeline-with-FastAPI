c8e650b4aaf8138349a9510ed33682b466927daf
Jasmine Smith <61719007+JasmineSmithWGU@users.noreply.github.com>
2025-02-03 00:10:56 -0600

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get('https://127.0.0.1:8000/')
+
 
 # TODO: print the status code
-# print()
+print(r.status_code)
 # TODO: print the welcome message
-# print()
+print(r.json())
 
 
 
@@ -30,9 +31,9 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post('https://127.0.0.1:8000/data/', json=data) # Your code here
 
 # TODO: print the status code
-# print()
+print(r.status_code)
 # TODO: print the result
-# print()
+print(r.json())
